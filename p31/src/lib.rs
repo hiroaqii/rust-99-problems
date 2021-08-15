fcb5752b4b6b8f01be1046cba9095a7cb02e7e32
hiroaqii <hiroaqii@gmail.com>
2021-08-15 22:00:20 +0900

    problem 31

---
+++ p31/src/lib.rs
@@ -0,0 +1,29 @@
+/// Determine whether a given integer number is prime.
+fn is_prime(n: usize) -> bool {
+    match n {
+        0 | 1 => false,
+        _ => {
+            for x in 2..=(n as f64).sqrt() as usize {
+                if n % x == 0 {
+                    return false;
+                }
+            }
+            true
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(is_prime(0), false);
+        assert_eq!(is_prime(1), false);
+        assert_eq!(is_prime(2), true);
+        assert_eq!(is_prime(11), true);
+        assert_eq!(is_prime(100), false);
+        assert_eq!(is_prime(233), true);
+    }
+}
