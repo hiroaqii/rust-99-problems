af8b7b6b2436b14a8d1354741ceb182808c823f1
hiroaqii <hiroaqii@gmail.com>
2021-08-17 01:28:02 +0900

    problem 34

---
+++ p34/src/lib.rs
@@ -0,0 +1,44 @@
+use fraction::{Fraction, ToPrimitive};
+use std::collections::HashSet;
+
+/// Calculate Euler's totient function phi(m).
+/// Euler's so-called totient function phi(m) is defined as the number of positive integers r (1 <= r < m) that are coprime to m.
+/// Example: m = 10: r = 1,3,7,9; thus phi(m) = 4. Note the special case: phi(1) = 1.
+
+pub fn prime_factorize(n: u32) -> Vec<u32> {
+    let mut tmp = n;
+    let mut ret: Vec<u32> = vec![];
+    for i in 2..=(n / 2 + 1) {
+        loop {
+            if tmp % i == 0 {
+                ret.push(i);
+                tmp /= i;
+            } else {
+                break;
+            }
+        }
+    }
+    ret
+}
+
+pub fn totient_phi(n: u32) -> u32 {
+    let set: HashSet<u32> = prime_factorize(n).into_iter().collect();
+    let mut ret = Fraction::from(n);
+    for x in set {
+        ret *= Fraction::from(1) - Fraction::new(1 as u32, x as u32);
+    }
+    ret.to_u32().unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(totient_phi(1), 1);
+        assert_eq!(totient_phi(6), 2);
+        assert_eq!(totient_phi(10), 4);
+        assert_eq!(totient_phi(12), 4);
+    }
+}
